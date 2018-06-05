c31b49e80dc48f8d58b8c5437ffb4dd477a331fb
Babajide Bantale <bantale@relianceinfosystems.com>
2018-06-05 12:02:03 +0100

    Updated

--- UnderstandingContext.js
+++ UnderstandingContext.js
@@ -94,8 +94,137 @@ function OnEstimatedClosedDateChange(){
     }
 }
 
+//Test
 Xrm.Page.getAttribute("estimatedclosedate").addOnChange(display);
 function display(){
     alert("Estimated Closed Date Changed");
+}
+
+function GetRecordId(){
+    var guidValue = Xrm.Page.data.entity.getId();
+    guidValue =  guidValue.replace("{", "").replace("}", "");
+    Xrm.Page.getAttribute("new_recordguid").setValue(guidValue);
+}
+
+
+function GetSelectedInterest(executionContext){
+    var formContext = executionContext.getFormContext();
+    var interest = formContext.getAttribute("new_interest");
+    var selectedVal = formContext.getAttribute("new_interestselected");
+
+    if(interest.getValue() != null)
+    {
+        var interestValues = interest.getValue();
+        var interestText = interest.getText();
+        var interestLength = interestValues.length;
+        //alert("Length: "+ interestLength);
+    
+
+        var s = "";
+        for(var i=0; i < interestLength; i++){
+
+            s += interestText[i] + "; ";
+        }
+        //alert(s);
+
+        //Does new_interestselected have value
+        if(selectedVal.getValue() != null){
+            selectedVal.setValue(null);
+        }
+
+        selectedVal.setValue(s);
+    }
+    else{
+        if(selectedVal.getValue() != null){
+            selectedVal.setValue(null);
+        }
+    }
+   
+}
+
+
+function GetSelectedDeptartment(executionContext){
+    var formContext = executionContext.getFormContext();
+    var department = formContext.getAttribute("new_multidepartment");
+    var selectedDept = formContext.getAttribute("new_departmentselected");
+
+    if(department.getValue() != null){
+
+        var deptValue = department.getValue();
+        var deptText = department.getText();
+        var deptLength = deptValue.length;
+        //alert("Length: "+ deptLength)
+
+        var d = "";
+        var selectedOptions = [];
+        for(var i=0; i < deptLength; i++){
+            selectedOptions.push(deptValue[i]);
+        }
+        selectedOptions.sort();
+        
+        //alert(selectedOptions);
+
+        for(var j=0; j < selectedOptions.length; j++){
+            d += selectedOptions[j];
+        }
+
+        //alert(d); 
+
+        if(selectedDept.getValue() != null){
+            selectedDept.setValue(null);
+        }
+
+        setTimeout(() => {
+            selectedDept.setValue(d);
+        }, 3000);
+           
+    }
+    else{
+        if(selectedDept.getValue() != null){
+            selectedDept.setValue(null);
+        }
+    }
+}
+
+//Test version
+function GetSelectedDeptartment(executionContext){
+    var formContext = executionContext.getFormContext();
+    var department = formContext.getAttribute("new_multidepartment");
+    var selectedDept = formContext.getAttribute("new_departmentselected");
+
+    if(department.getValue() != null){
+
+        var deptValue = department.getValue();
+        var deptText = department.getText();
+        var deptLength = deptValue.length;
+        //alert("Length: "+ deptLength)
+
+        
+        var d = "";
+        var selectedOptions = [];
+        for(var i=0; i < deptLength; i++){
+            selectedOptions.push(deptValue[i]);
+        }
+        selectedOptions.sort();
+        
+        //alert(selectedOptions);
+
+        for(var j=0; j < selectedOptions.length; j++){
+            d += selectedOptions[j];
+        }
+
+        //alert(d); 
+
+        if(selectedDept.getValue() != null){
+            selectedDept.setValue(null);
+        }
+
+        selectedDept.setValue(d);
+    }
+    else{
+        if(selectedDept.getValue() != null){
+            selectedDept.setValue(null);
+        }
+    }
 }
 //# sourceURL=dynamicScript.js
