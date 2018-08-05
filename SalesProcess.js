b0309f82b7b2d71debeb2e9944d1c3011b54d0a6
Babajide Bantale <bantale@relianceinfosystems.com>
2018-08-05 02:03:06 +0100

    Sales Process Scripts

--- SalesProcess.js
+++ SalesProcess.js
@@ -20,7 +20,7 @@ function OpenProcurementForm(){
 
     parameters["customer_id"] = accountid;
     parameters["customer_idname"] = accountName;
-    parameters["quote_id"] = quoteId;
+    parameters["customer_idtype"] = entityType;
 
     //Open Form
     Xrm.Utility.openEntityForm("new_procurement", null, parameters, windowOptions);
@@ -31,19 +31,20 @@ function LoadValueProcurementForm(){
     var param = Xrm.Page.context.getQueryStringParameters();
     var accountid = param["customer_id"];
     var accountName = param["customer_idname"];
-    var accountType = param["customer_idtype"];
+    var acctentityType = param["customer_idtype"];
 
     //Populate the Account if there is one
-    if(accountid != null || account != undefined){
+    if(accountid != null || accountid != undefined){
         Xrm.Page.getAttribute("new_account").setValue(
             [{
                id: accountid,
                name: accountName,
-               entityType: accountType
+               entityType: acctentityType
             }]
         );
     }
 }
+
 "https://sbsengg.crm4.dynamics.com/main.aspx?etn=rel_procurement&pagetype=entityrecord";
 
 
