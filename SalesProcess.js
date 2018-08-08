f39d4eebdba78f780660af4ea67d15366738b380
Babajide Bantale <bantale@relianceinfosystems.com>
2018-08-08 12:52:09 +0100

    Update to SalesProcess and Added a new js file

--- SalesProcess.js
+++ SalesProcess.js
@@ -1,11 +1,4 @@
-function OpenProcurementForm(){
-    var windowOptions = {
-        openInNewWindow: true
-       };
-    var paramters = {};
-    Xrm.Utility.openEntityForm("rel_procurement", null, null, windowOptions);
-}
-
+//Open a Procurement Form and pass parameters
 function OpenProcurementForm(){
     var windowOptions = {
         openInNewWindow: true
@@ -16,22 +9,26 @@ function OpenProcurementForm(){
     var accountid = Xrm.Page.getAttribute("customerid").getValue()[0].id;
     var accountName = Xrm.Page.getAttribute("customerid").getValue()[0].name;
     var entityType = Xrm.Page.getAttribute("customerid").getValue()[0].entityType;
+
     var quoteId = Xrm.Page.data.entity.getId();
 
     parameters["customer_id"] = accountid;
     parameters["customer_idname"] = accountName;
     parameters["customer_idtype"] = entityType;
+    parameters["quote_id"] = quoteId;
 
     //Open Form
     Xrm.Utility.openEntityForm("new_procurement", null, parameters, windowOptions);
 }
 
+//Set Default Values onLoad of Procurement Form
 function LoadValueProcurementForm(){
     // Get the Value of the Account through the Custom Parameters
     var param = Xrm.Page.context.getQueryStringParameters();
     var accountid = param["customer_id"];
     var accountName = param["customer_idname"];
     var acctentityType = param["customer_idtype"];
+    var quoteid = param["quote_id"];
 
     //Populate the Account if there is one
     if(accountid != null || accountid != undefined){
@@ -43,9 +40,18 @@ function LoadValueProcurementForm(){
             }]
         );
     }
+
+    //Populate the Quote
+    if(quoteid != null || quoteid != undefined)
+    {
+        Xrm.Page.getAttribute("new_quote").setValue(
+            [{
+                id: quoteid,
+                entityType: "quote"
+            }]);    
+    }
 }
 
-"https://sbsengg.crm4.dynamics.com/main.aspx?etn=rel_procurement&pagetype=entityrecord";
 
 
 
